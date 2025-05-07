c2223af8ec7a28706f352f2ab7ceef0eed9c4687
jonathan <jonathan20cba@gmail.com>
2025-05-06 23:28:13 -0300

    Se agrega confg del servidor

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import "dotenv/config";
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (_: Request, res: Response) => {
+  res.send("Bienvenido a la API de seguimiento de entrenamiento");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en localhost://${port}`);
+});
