1e2d2c3fd4d9540bcb1eab0848646dc083d57d4a
jonathan <jonathan20cba@gmail.com>
2025-05-17 11:19:53 -0300

    Se agregan las validaciones para crear entrenamiento

--- src/routes/workout.route.ts
+++ src/routes/workout.route.ts
@@ -2,9 +2,10 @@ import express from "express";
 import { Router } from "express";
 import { checkAuth } from "../middleware/checkAuth";
 import { createWorkout } from "../controllers/workout.controller";
+import { workoutValidator } from "../validators/workout.validator";
 
 const router: Router = express.Router();
 
-router.post("/", checkAuth, createWorkout);
+router.post("/", checkAuth, workoutValidator, createWorkout);
 
 export default router;
---
+++ src/validators/workout.validator.ts
@@ -0,0 +1,61 @@
+import { Response, Request, NextFunction } from "express";
+import { check, ValidationChain } from "express-validator";
+import { validateResults } from "../utils/handleValidator";
+
+export const workoutValidator: (
+  | ValidationChain
+  | ((req: Request, res: Response, next: NextFunction) => void)
+)[] = [
+  check("scheduledDate")
+    .exists()
+    .withMessage("El campo scheduledDate debe existir")
+    .notEmpty()
+    .withMessage("El campo scheduledDate no debe estar vacío")
+    .isISO8601()
+    .withMessage(
+      "El campo scheduledDate debe ser una fecha válida en formato ISO8601 (YYYY-MM-DDTHH:MM:SSZ)"
+    ),
+  check("note")
+    .optional()
+    .isString()
+    .withMessage("El campo note debe ser un string")
+    .isLength({ max: 255 })
+    .withMessage("El campo note no debe superar los 255 caracteres"),
+  check("exercises")
+    .exists()
+    .withMessage("El array de exercises debe existir")
+    .isArray({ min: 1 })
+    .withMessage("El array de exercises debe tener al menos un ejercicio"),
+  check("exercises.*.exerciseId")
+    .exists()
+    .withMessage(
+      "El campo exerciseId debe existir dentro del array de ejercicios"
+    )
+    .notEmpty()
+    .withMessage("El campo exerciseId no debe estar vacío")
+    .isInt({ min: 1 })
+    .withMessage("El campo exerciseId debe ser un entero positivo"),
+  check("exercises.*.sets")
+    .exists()
+    .withMessage("El campo sets dentro del array debe existir")
+    .notEmpty()
+    .withMessage("El campo sets no debe estar vacío")
+    .isInt()
+    .withMessage("El campo sets debe ser un entero"),
+  check("exercises.*.reps")
+    .exists()
+    .withMessage("El campo reps dentro del array de ejercicios debe existir")
+    .notEmpty()
+    .withMessage("El campo reps no debe estar vacío")
+    .isInt({ min: 1 })
+    .withMessage("El campo reps debe ser un entero positivo"),
+  check("exercises.*.weightKg")
+    .optional()
+    .isFloat({ min: 0 })
+    .withMessage(
+      "El campo weightKg dentro del array de ejercicios debe ser númerico positivo"
+    ),
+  (req: Request, res: Response, next: NextFunction) => {
+    return validateResults(req, res, next);
+  },
+];
